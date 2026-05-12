cf837139ef481ef3eb978f56b167b5c7c64754c5
duan-deqing <2030851914@qq.com>
2026-05-12 08:59:58 +0800

    note: add 08,09,10

--- docs/_code/python/07-模块与包/模块搜索路径与命名空间管理.py
+++ docs/_code/python/07-模块与包/模块搜索路径与命名空间管理.py
@@ -6,3 +6,10 @@ print(sys.path)
 
 import math
 print(dir(math)) 
+
+def total_sum(data):
+    return sum(data)
+
+print(total_sum([1,2,3]))        # 列表
+print(total_sum((4,5,6)))        # 元组
+print(total_sum({7,8,9}))        # 集合
